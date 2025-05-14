3a23a4f4b74e10bbd2b4bb824522221c3901d104
haticekartal <haticenaz_2001@hotmail.com>
2025-05-14 14:12:13 +0300

    hava durumu uygulaması tamamlandi.

---
+++ WeatherApp/ForecastTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ForecastTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Hatice Kartal on 3.05.2025.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
